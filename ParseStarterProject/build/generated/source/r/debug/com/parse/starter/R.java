8491549bf7fd6352b2360be9f8ea0633e489e58a
dartmapthem <dartmapthem@gmail.com>
2015-08-08 08:43:33 -0700

    Still working on centering spinners on find a group page

--- ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -27,11 +27,16 @@ public final class R {
         public static final int navigation_drawer_width=0x7f050002;
     }
     public static final class drawable {
-        public static final int drawer_shadow=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int iconholder=0x7f020003;
-        public static final int infoimg=0x7f020004;
+        public static final int date=0x7f020000;
+        public static final int detailbackground1=0x7f020001;
+        public static final int drawer_shadow=0x7f020002;
+        public static final int group=0x7f020003;
+        public static final int ic_drawer=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int iconholder=0x7f020006;
+        public static final int infoimg=0x7f020007;
+        public static final int location=0x7f020008;
+        public static final int time=0x7f020009;
     }
     public static final class id {
         public static final int ClassTxt=0x7f080000;
